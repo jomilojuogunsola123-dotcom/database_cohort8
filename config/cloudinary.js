e1438197387accdfa168f490851111670c618a34
jomilojuogunsola123-dotcom <jomilojuogunsola123@gmail.com>
2026-09-12 18:18:20 +0100

    Another Commit

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 //import {v2 as cloudinary} from 'cloudinary(this is how you import usinf mgs)
 
 cloudinary.config({
-    cloud_name : "tio8ftqg",
-    api_key : "677691593892891",
-    api_secret : "ZWn6xpl2Rxjo083SSI9Sd8KDuG0"
+    cloud_name : process.env.CLOUD_NAME,
+    api_key : process.env.API_KEY,
+    api_secret : process.env.API_SECRET
 })
 
 module.exports = cloudinary;
